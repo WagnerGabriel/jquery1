08d610cfceee70710f5dda649360b545436020f3
Patricksoares556 <44869349+Patricksoares556@users.noreply.github.com>
2019-07-01 12:19:25 -0300

    Aula Angular de segunda

---
+++ src/app/photo-list/photo-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector:"Photo-list-app",
+    templateUrl: "photo-list.component.html"
+})
+
+
+export class PhotoListComponent {}
